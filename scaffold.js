f7c22d68408b82274d0f98219dafa17ca0e9745e
Michael Jennings <mpjngs@gmail.com>
2015-01-27 17:35:03 -0800

    fixed bug in scaffold building algorithm

--- scaffold.js
+++ scaffold.js
@@ -19,24 +19,29 @@ function scaffold(center, radius, radWidth, lineWidth, areas, spots){
 
     nwareas = insertField(nwareas, [start, end]);
 
-    if(nwareas[nwareas.length-1][1] > Math.PI * 2){
-      start = 0;
-      end = nwareas[nwareas.length-1][1] - Math.PI * 2;
-      
-      nwareas = insertField(nwareas, [start, end]);
-    }
-    if(nwareas[0][0] < 0){
-      start = nwareas[0][0] + Math.PI * 2;
-      end = Math.PI * 2;
-
-      nwareas = insertField(nwareas, [start, end]);
-    }
-
-
     lineout(center, radius, radWidth - lineWidth, lineWidth, seeds[j])([100,100,100, 255]);
     myArc(center, radius + radWidth * .5, seeds[j] - .2, lineWidth, .4, 0)([100,100,100, 255]);
   }
 
+  if(nwareas.length > 0 && nwareas[nwareas.length-1][1] > Math.PI * 2){
+    start = 0;
+    end = nwareas[nwareas.length-1][1] - Math.PI * 2;
+
+    nwareas[nwareas.length-1][1] = Math.PI * 2;    
+
+    nwareas = insertField(nwareas, [start, end]);
+  }
+  if(nwareas.length > 0 && nwareas[0][0] < 0){
+    start = nwareas[0][0] + Math.PI * 2;
+    end = Math.PI * 2;
+
+    nwareas[0][0] = 0;
+
+    nwareas = insertField(nwareas, [start, end]);
+  }
+
+  console.log(seeds);
+  console.log(nwareas);
   return nwareas;
 }
 
@@ -49,6 +54,9 @@ function insertField(arr, field){
 
   if(k === 0 || start > arr[k-1][1]){
     arr.splice(k, 0, [start, end]);
+  } else {
+    k--;
+    arr[k][1] = Math.max(arr[k][1], end);
   }
   while(k+1 < arr.length && end > arr[k+1][0]){
     if(end < arr[k+1][1]){
